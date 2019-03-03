8c714727eee0fa2560aa07ab446573e3c5a8c873
hntk03 <hntk03@gmail.com>
2019-03-03 15:09:37 +0900

    add multi thread func

--- category_encoder.py
+++ category_encoder.py
@@ -30,9 +30,11 @@
 # ---
 
 import gc
+import logging
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from joblib import Parallel, delayed
 
 
 
@@ -41,7 +43,7 @@ from sklearn.preprocessing import LabelEncoder
 class CategoryEncoder:
     
         
-    def columns_type(self):
+    def __columns_type(self):
         
         feats = self.__feats
         category_columns = self.__category_columns
@@ -78,14 +80,34 @@ class CategoryEncoder:
         columns.remove(id_column)
         columns.remove(target_column)
         self.__feats = columns
-        self.columns_type()
+        self.__columns_type()
         
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        logger.addHandler(ch)
+        self.__logger = logger
+        
+    def __drop_columns(self, category_columns):
+        
+        train_df = self.__train_df
+        test_df = self.__test_df
+        
+        for col in category_columns:
+            train_df.drop(col, axis=1, inplace=True)
+            test_df.drop(col, axis=1, inplace=True)
+            
+        gc.collect()
+        
+        self.__train_df = train_df
+        self.__test_df = test_df
     
     def get_columns_type(self):
         
         return self.__numeric_columns, self.__category_columns
     
-    def fill_nan(self, category_columns):
+    def __fill_nan(self, category_columns):
         
         train_df = self.__train_df
         test_df = self.__test_df
@@ -96,9 +118,78 @@ class CategoryEncoder:
             
         self.__train_df = train_df 
         self.__test_df = test_df 
-        
     
-    def encoding(self, mycategory_columns=None):
+    def __encoder(self, col):
+        
+        feats = []
+        train_shape = self.__train_shape
+        test_shape = self.__test_shape
+        
+        traincol_series = self.__train_df[col] 
+        testcol_series = self.__test_df[col] 
+        
+        _train_ce = np.zeros(train_shape[0], dtype=int)
+        _test_ce = np.zeros(test_shape[0], dtype=int)
+        
+        _train_lce = np.zeros(train_shape[0], dtype=int)
+        _test_lce = np.zeros(test_shape[0], dtype=int)
+        
+        
+        train_df = pd.DataFrame()
+        test_df = pd.DataFrame()
+        
+        le = LabelEncoder()
+
+        # Label Encoding
+        _train = traincol_series.values
+        _test = testcol_series.values
+        _all = np.r_[_train, _test]
+
+        le.fit(_all)
+        _train_le = le.transform(_train)
+        _test_le = le.transform(_test)
+
+        col_LE = col + '_LE'
+        train_df[col_LE] = _train_le
+        test_df[col_LE] = _test_le
+        feats.append(col_LE)
+
+        # Count Encoding and Label Count Encoding
+        value_counts = train_df[col_LE].value_counts()
+        _index = value_counts.index.values
+        rank = pd.Series(np.arange(_index.shape[0], 0, -1), index=_index)
+
+        #train
+        for iter, value in enumerate(_train_le):
+            _train_ce[iter] = value_counts[value]
+            _train_lce[iter] = rank[value]
+
+        _set_index  = set(_index)
+        for iter, value in enumerate(_test_le):
+            if set([value]) <= _set_index:
+                _test_ce[iter] = value_counts[value]
+                _test_lce[iter] = rank[value]
+            else:
+                _test_ce[iter] = 1
+                _test_lce[iter] = 1
+
+
+        col_CE = col + '_CE'
+        train_df[col_CE] = _train_ce
+        test_df[col_CE] = _test_ce
+        feats.append(col_CE)
+
+        col_LCE = col + '_LCE'
+        train_df[col_LCE] = _train_lce
+        test_df[col_LCE] = _test_lce
+        feats.append(col_LCE)
+        
+        return train_df, test_df, feats
+
+    
+    def encoding(self, mycategory_columns=None, n_jobs=-1):
+        
+        logger = self.__logger
         
         feats = self.__feats
         train_shape = self.__train_shape
@@ -109,7 +200,7 @@ class CategoryEncoder:
         else:
             category_columns = mycategory_columns
             
-        self.fill_nan(category_columns) 
+        self.__fill_nan(category_columns) 
         
         train_df = self.__train_df
         test_df = self.__test_df
@@ -121,62 +212,22 @@ class CategoryEncoder:
         _train_lce = np.zeros(train_shape[0], dtype=int)
         _test_lce = np.zeros(test_shape[0], dtype=int)
         
-        for col in category_columns:
+        encoder = self.__encoder 
+        result = (Parallel(n_jobs=n_jobs, verbose=3)([delayed(encoder)( x) for x in category_columns]))
+                    
+        for iter, r in enumerate(result):
             
-            le = LabelEncoder()
-            
-            # Label Encoding
-            _train = train_df[col].values
-            _test = test_df[col].values
-            _all = np.r_[_train, _test]
-            
-            le.fit(_all)
-            _train_le = le.transform(_train)
-            _test_le = le.transform(_test)
-            
-            col_LE = col + '_LE'
-            train_df[col_LE] = _train_le
-            test_df[col_LE] = _test_le
-            feats.append(col_LE)
-            
-            train_df.drop(col, axis=1, inplace=True)
-            test_df.drop(col, axis=1, inplace=True)
-            gc.collect()
-            
-            # Count Encoding and Label Count Encoding
-            value_counts = train_df[col_LE].value_counts()
-            _index = value_counts.index.values
-            rank = pd.Series(np.arange(_index.shape[0], 0, -1), index=_index)
-            
-            #train
-            for iter, value in enumerate(_train_le):
-                _train_ce[iter] = value_counts[value]
-                _train_lce[iter] = rank[value]
-                
-            _set_index  = set(_index)
-            for iter, value in enumerate(_test_le):
-                if set([value]) <= _set_index:
-                    _test_ce[iter] = value_counts[value]
-                    _test_lce[iter] = rank[value]
-                else:
-                    _test_ce[iter] = 1
-                    _test_lce[iter] = 1
+            train_df = pd.concat([train_df, r[0]], axis=1)
+            test_df = pd.concat([train_df, r[1]], axis=1)
+            feats.extend(r[2])
             
             
-            col_CE = col + '_CE'
-            train_df[col_CE] = _train_ce
-            test_df[col_CE] = _test_ce
-            feats.append(col_CE)
-            
-            col_LCE = col + '_LCE'
-            train_df[col_LCE] = _train_lce
-            test_df[col_LCE] = _test_lce
-            feats.append(col_LCE)
-            
-        
         self.__train_df = train_df
         self.__test_df = test_df
         self.__feats = feats
+                
+        self.__drop_columns(category_columns)
+        
         
         
     def get_df(self):
@@ -188,9 +239,6 @@ class CategoryEncoder:
         return train_df, test_df, feats
         
             
-                    
-                    
-                
                 
                 
             
