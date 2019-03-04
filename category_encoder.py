7639981867c340cf737226dbb41bbb078c7f2360
hntk03 <hntk03@gmail.com>
2019-03-04 11:44:06 +0900

    add memory reduce

--- category_encoder.py
+++ category_encoder.py
@@ -266,6 +266,9 @@ class CategoryEncoder:
             test_df = pd.concat([test_df, r[1]], axis=1)
             feats.extend(r[2])
             
+        train_df = self.__reduce_mem_usage(train_df)
+        test_df = self.__reduce_mem_usage(test_df)
+            
             
         self.__train_df = train_df
         self.__test_df = test_df
@@ -352,7 +355,34 @@ class CategoryEncoder:
         return train_df, test_df, feats
 
         
-            
+    def __reduce_mem_usage(self, df):
+        start_mem = df.memory_usage().sum() / 1024**2
+
+        for col in df.columns:
+            col_type = df[col].dtype
+            if col_type != 'object' and col_type != 'datetime64[ns]':
+                c_min = df[col].min()
+                c_max = df[col].max()
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)
+                else:
+                    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        df[col] = df[col].astype(np.float32)  # feather-format cannot accept float16
+                    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        df[col] = df[col].astype(np.float64)
+
+        end_mem = df.memory_usage().sum() / 1024**2
+        
+        return df
                 
                 
             
